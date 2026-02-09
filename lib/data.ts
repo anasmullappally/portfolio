0ac37369d93c76a7ff5b8a7238bcd4c6ecdb98ce
anasmullappally <mohammedanas362@gmail.com>
2026-02-09 13:22:59 +0530

    basic outline

---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+export const GENERAL_INFO = {
+    email: "anasmullappally.dev@gmail.com",
+    emailSubject: "Let's collaborate on a project",
+    emailBody:
+        "Hi Anas, I came across your portfolio and would love to discuss a potential project with you.",
+};
+
+export const SOCIAL_LINKS = [
+    { name: 'github', url: 'https://github.com/anasmullappally' },
+    { name: 'linkedin', url: 'https://www.linkedin.com/in/anasmullappally/' },
+    { name: 'instagram', url: 'https://www.instagram.com/anas.mullappally/' },
+    { name: 'facebook', url: 'https://www.facebook.com/' },
+];
